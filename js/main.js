580e4321172a6936715f7cba5061faa4c6f27f2d
Konrad Fedorczyk <contact@realhe.ro>
2021-12-09 09:33:04 +0100

    Update files for lesson 4

--- js/main.js
+++ js/main.js
@@ -1,9 +1,42 @@
-document.addEventListener("click", function(e) {
+const keys = document.querySelector("#keys");
+const cats = document.querySelector("#cats");
+
+
+const toggleCat = (note, state = true) => {
+  console.log(cats);
+
+  cats.querySelector(`li[data-note=${note}]`).classList.toggle("-animate", state);
+}
+
+keys.addEventListener("mousedown", (e) => {
   const clikedElement = e.target;
 
   if (clikedElement.matches("li.key")) {
-    console.log(clikedElement.dataset.note);
+    const { note } =  clikedElement.dataset;
+    console.log("start", note);
+    toggleCat(note);
   } else {
     console.log("Other element clicked", clikedElement);
   }
+});
+
+document.addEventListener("mouseup", () => {
+  const allCats = [...cats.querySelectorAll("li")];
+
+  console.log("Muting down all cats");
+  allCats.map((elm) => elm.classList.remove("-animate"));
+});
+
+
+let isKeyDown = false
+
+document.addEventListener("keydown", (e) => {
+  if (!isKeyDown) {
+    console.log("key pressed", e.code);
+    isKeyDown = true;
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  isKeyDown = false;
 });
