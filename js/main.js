9df77c6c223ad12d178c5164d8a2fcd10e853242
Konrad Fedorczyk <contact@realhe.ro>
2021-12-16 11:36:50 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
 const keys = document.querySelector("#keys");
 const cats = document.querySelector("#cats");
 
+const pitchMap = {
+  "C": 1,
+  "D": 2,
+  "E": 3,
+  "F": 4,
+  "G": 5,
+  "A": 6,
+  "B": 7,
+  "Cf": 1.5,
+  "Df": 2.5,
+  "Ff": 4.5,
+  "Gf": 5.5,
+  "Af": 6.5
+};
+
+const playNote = (note) => {
+  const meow = new Audio("../assets/meow.mp3");
+
+  if (pitchMap[note]) {
+    meow.playbackRate = pitchMap[note];
+  }
+
+  meow.play();
+}
+
 /*
 const toggleCat = function (note, state = true) {
 
@@ -16,6 +41,15 @@ const toggleCat = (note, animate = true) => {
     .classList.toggle("-animate", animate);
 };
 
+const toggleKey = (note, enable = true) => {
+  keys
+  .querySelector(`li[data-note=${note}]`)
+  .classList.toggle("-active", enable);
+}
+
+/**
+ * Mouse handlers
+ */
 keys.addEventListener("mousedown", (e) => {
   // const target = e.target; the same!
   const { target } = e;
@@ -26,6 +60,7 @@ keys.addEventListener("mousedown", (e) => {
     console.log("Played note", note);
 
     toggleCat(note);
+    playNote(note);
   }
 });
 
@@ -66,16 +101,55 @@ if (isKeyDown === false) {}
 
 */
 
-let isKeyDown = false;
+const keyMap = {
+  KeyZ: "C",
+  KeyX: "D",
+  KeyC: "E",
+  KeyV: "F",
+  KeyB: "G",
+  KeyN: "A",
+  KeyM: "B",
+  KeyS: "Cf",
+  KeyD: "Df",
+  KeyG: "Ff",
+  KeyH: "Gf",
+  KeyJ: "Af"
+};
 
+const isKeyDown = [];
+
+// Key press
 document.addEventListener("keydown", (e) => {
-  if (!isKeyDown) {
-    console.log("Key pressed", e.code);
-    isKeyDown = true;
+  const { code } = e;
+
+  if (!isKeyDown[code]) {
+    console.log("Key pressed", code)
+    console.time(`key_${code}`);
+
+    if (keyMap[code]) {
+      const note = keyMap[code];
+      console.log("Played note", note);
+      toggleCat(note);
+      toggleKey(note);
+    }
+
+    isKeyDown[code] = true;
   }
 });
 
+// Key depress
 document.addEventListener("keyup", (e) => {
-  isKeyDown = false;
+  const { code } = e;
+
+  console.log("Key depressed", code);
+  console.timeEnd(`key_${code}`);
+
+  if (keyMap[code]) {
+    const note = keyMap[code];
+    toggleCat(note, false);
+    toggleKey(note, false);
+  }
+
+  isKeyDown[code] = false;
 });
 
